601bd803b2988a43e0be94fbccfad73eead978ce
nerhed2makob <nerhed2makob@makobj.store>
2024-05-29 10:06:57 +0530

    main

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-$(window).on('load', function() {
+$(window).on('load', function () {
 	/*------------------
 		Preloder
 	--------------------*/
@@ -17,26 +17,26 @@ $(window).on('load', function() {
 
 });
 
-(function($) {
+(function ($) {
 	/*------------------
 		Navigation
 	--------------------*/
 	$(".main-menu").slicknav({
-        appendTo: '.header-section',
+		appendTo: '.header-section',
 		allowParentLinks: true,
 		closedSymbol: '<i class="fa fa-angle-right"></i>',
 		openedSymbol: '<i class="fa fa-angle-down"></i>'
 	});
-	
+
 	/*----------------
 		Search model
 	------------------*/
-	$('#search-switch').on('click', function() {
+	$('#search-switch').on('click', function () {
 		$('.search-model').fadeIn(400);
 	});
 
-	$('.search-close-switch').on('click', function() {
-		$('.search-model').fadeOut(400,function(){
+	$('.search-close-switch').on('click', function () {
+		$('.search-model').fadeOut(400, function () {
 			$('#search-input').val('');
 		});
 	});
@@ -44,12 +44,12 @@ $(window).on('load', function() {
 	/*---------------
 		Infor model
 	----------------*/
-	$('#infor-switch').on('click', function() {
+	$('#infor-switch').on('click', function () {
 		$('.infor-model-warp').fadeIn(400);
-		$('.infor-model-warp').addClass('active');	
+		$('.infor-model-warp').addClass('active');
 	});
 
-	$('.infor-close').on('click', function() {
+	$('.infor-close').on('click', function () {
 		$('.infor-model-warp').removeClass('active');
 		$('.infor-model-warp').fadeOut(400);
 	});
@@ -58,7 +58,7 @@ $(window).on('load', function() {
 	/*------------------
 		Background Set
 	--------------------*/
-	$('.set-bg').each(function() {
+	$('.set-bg').each(function () {
 		var bg = $(this).data('setbg');
 		$(this).css('background-image', 'url(' + bg + ')');
 	});
@@ -67,18 +67,18 @@ $(window).on('load', function() {
 	/*------------------
 		Back to top
 	--------------------*/
-	$(window).scroll(function() {
+	$(window).scroll(function () {
 		if ($(this).scrollTop() >= 500) {
 			$('.back-to-top').fadeIn();
-			$('.back-to-top').css('display','flex');
+			$('.back-to-top').css('display', 'flex');
 		} else {
 			$('.back-to-top').fadeOut();
 		}
 	});
 
-	$(".back-to-top").click(function() {
-		$("html, body").animate({scrollTop: 0}, 1000);
-	 });
+	$(".back-to-top").click(function () {
+		$("html, body").animate({ scrollTop: 0 }, 1000);
+	});
 
 
 
@@ -117,14 +117,14 @@ $(window).on('load', function() {
 		dots: true,
 		margin: 30,
 		autoplay: true,
-		responsive : {
-			0 : {
+		responsive: {
+			0: {
 				items: 1
 			},
-			768 : {
+			768: {
 				items: 2
 			},
-			1170 : {
+			1170: {
 				items: 3,
 			}
 		},
@@ -138,13 +138,13 @@ $(window).on('load', function() {
 		nav: true,
 		dots: false,
 		margin: 0,
-		navText:['<i class="material-icons">keyboard_arrow_left</i>','<i class="material-icons">keyboard_arrow_right</i>'],
+		navText: ['<i class="material-icons">keyboard_arrow_left</i>', '<i class="material-icons">keyboard_arrow_right</i>'],
 		autoplay: true,
-		responsive : {
-			0 : {
+		responsive: {
+			0: {
 				items: 1
 			},
-			768 : {
+			768: {
 				items: 2
 			},
 		},
@@ -158,13 +158,13 @@ $(window).on('load', function() {
 		nav: true,
 		dots: false,
 		margin: 0,
-		navText:['<i class="material-icons">keyboard_arrow_left</i>','<i class="material-icons">keyboard_arrow_right</i>'],
+		navText: ['<i class="material-icons">keyboard_arrow_left</i>', '<i class="material-icons">keyboard_arrow_right</i>'],
 		autoplay: true,
-		responsive : {
-			0 : {
+		responsive: {
+			0: {
 				items: 1
 			},
-			768 : {
+			768: {
 				items: 2
 			},
 		},
@@ -177,16 +177,16 @@ $(window).on('load', function() {
 		loop: true,
 		nav: true,
 		dots: false,
-		navText:[' ',' '],
+		navText: [' ', ' '],
 		autoplay: true,
-		responsive : {
-			0 : {
+		responsive: {
+			0: {
 				items: 1
 			},
-			768 : {
+			768: {
 				items: 1
 			},
-			992 : {
+			992: {
 				items: 2,
 			}
 		},
@@ -195,26 +195,32 @@ $(window).on('load', function() {
 	/*------------------
 		Gallery Slider
 	--------------------*/
-	$('.gallery-slider').owlCarousel({
-		loop: true,
-		nav: false,
-		dots: false,
-		items: 6,
-		responsive : {
-			0 : {
-				items: 2
+	$(document).ready(function () {
+		$('.gallery-slider').owlCarousel({
+			loop: true,
+			nav: false,
+			dots: false,
+			autoplay: true,
+			autoplayTimeout: 2000, // Adjust this value to change the delay between slides (2000ms = 2 seconds)
+        autoplaySpeed: 1000, 
+			items: 6,
+			responsive: {
+				0: {
+					items: 2
+				},
+				475: {
+					items: 3
+				},
+				768: {
+					items: 4,
+				},
+				992: {
+					items: 6,
+				}
 			},
-			475 : {
-				items: 3
-			},
-			768 : {
-				items: 4,
-			},
-			992 : {
-				items: 6,
-			}
-		},
+		});
 	});
+
 
 	/*------------------
 		Popular Slider
@@ -229,36 +235,36 @@ $(window).on('load', function() {
 	/*------------------
 		Progress Bar
 	--------------------*/
-	$('.progress-bar-style').each(function() {
+	$('.progress-bar-style').each(function () {
 		var progress = $(this).data("progress");
 		var bgcolor = $(this).data("bgcolor");
 		var prog_width = progress + '%';
 		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '; background: '+ bgcolor +';"><span>' + prog_width + '</span></div>');
+			$(this).append('<div class="bar-inner" style="width:' + prog_width + '; background: ' + bgcolor + ';"><span>' + prog_width + '</span></div>');
 		}
 		else {
-			$(this).append('<div class="bar-inner" style="width:100%; background: '+ bgcolor +';"><span>100%</span></div>');
+			$(this).append('<div class="bar-inner" style="width:100%; background: ' + bgcolor + ';"><span>100%</span></div>');
 		}
 	});
 
 	/*------------------
-        Magnific Popup
-    --------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
+		Magnific Popup
+	--------------------*/
+	$('.video-popup').magnificPopup({
+		type: 'iframe'
+	});
 
 	/*--------------
-       Nice Select
-    ----------------*/
+	   Nice Select
+	----------------*/
 	$('#language').niceSelect();
 	$('.circle-select').niceSelect();
 
 	/*------------------
 		Datepicker
 	--------------------*/
-	$( ".event-date" ).datepicker();
-	
+	$(".event-date").datepicker();
+
 
 })(jQuery);
 
